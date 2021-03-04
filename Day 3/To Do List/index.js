6f16a9e1edce6b31e66c1206c036d008b44061dc
gsidburycrawford <66580946+gsidburycrawford@users.noreply.github.com>
2021-03-04 16:46:01 -0500

    Added comments

--- Day 3/To Do List/index.js
+++ Day 3/To Do List/index.js
@@ -12,20 +12,32 @@
 // Extra Credit: - When a list item is clicked, cross it out, then remove it after 1 second.
 // **Submit: A link to your github repo for the project as well as screenshots of the project working in the browser
 
-let task = document.querySelector('li');
-let taskList = document.querySelector('#tasks');
-let newTask = document.querySelector('button');
 
+let taskList = document.querySelector('#tasks'); // This is our unorderered list of tasks
+let newTask = document.querySelector('button'); // This is the submit button for adding a new class
+
+
+/*
+ When an item in the taskList is clicked, the event function targets the child 
+object of the list, and removes it.
+*/
 taskList.addEventListener('click', function(event){
-	event.target.remove();
+    event.target.remove();
 });
 
 
 
 newTask.addEventListener('click', function(event){
-    var li = document.createElement('li');
-	li.innerHTML = document.querySelector('input').value;
+    // We declare a new list object
+    var li = document.createElement('li'); 
+	
+    // We set the value of the list object to the text in the input box
+    li.innerHTML = document.querySelector('input').value;
+
+    // Add the list object to the to-do list.
 	taskList.appendChild(li);
+
+    // Remove the text in the input box
     document.querySelector('input').value = null;
 
 });
